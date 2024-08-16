dc69f217438dc1c3fb633ca24f6912bcf862d5f1
prkrtg <pgupta@caltech.edu>
2024-08-16 09:02:55 -0700

    COI-23 Enable Image subtraction mode for FITSViewer (#3)

--- fits_viewer_ui.py
+++ fits_viewer_ui.py
@@ -1,4 +1,14 @@
-from PyQt5.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QSlider, QTextEdit, QFileDialog, QWidget, QSplitter
+# -----------------------------------------------------------------------------
+# @file     fits_viewer_ui.py
+# @brief    The FITSViewerUI Class handles the UI components and layout of the FITS Viewer. 
+# @author   Prakriti Gupta <pgupta@astro.caltech.edu>
+# -----------------------------------------------------------------------------
+
+# pylint
+# pylint: disable=line-too-long
+
+from PyQt5.QtWidgets import (QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QLabel, QTextEdit, 
+                             QSplitter, QSlider, QAction, QFileDialog, QMenuBar, QPushButton, QTabWidget)
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap, QImage
 
@@ -6,83 +16,125 @@ class FITSViewerUI(QMainWindow):
     """
     Handles the UI components and layout of the FITS Viewer application.
     """
-    
     def __init__(self):
         super().__init__()
-
-        # Initialize UI components
         self.setup_ui()
 
     def setup_ui(self):
         """
         Sets up the user interface of the FITSViewer.
         """
-        # Set up the window
         self.setWindowTitle("FITS Image Viewer")
-        self.setGeometry(100, 100, 1000, 1000)  # Default size
+        self.setGeometry(100, 100, 1200, 1000)
 
-        # Set up the central widget and layout
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
         self.main_layout = QVBoxLayout()
         self.central_widget.setLayout(self.main_layout)
 
-        # Create a horizontal layout for the buttons
-        self.button_layout = QHBoxLayout()
+        self.create_tab_widget()
+        self.create_image_tab()
+        self.create_header_tab()
+        self.create_menu_bar()
 
-        # Create and add buttons to the horizontal layout
-        self.open_file_button = QPushButton("Open FITS Image")
-        self.open_file_button.clicked.connect(self.open_fits_image)
-        self.button_layout.addWidget(self.open_file_button)
+    def create_tab_widget(self):
+        """
+        Creates and configures the tab widget.
+        """
+        self.tab_widget = QTabWidget()
+        self.main_layout.addWidget(self.tab_widget)
 
-        self.open_directory_button = QPushButton("Open Directory of FITS Images")
-        self.open_directory_button.clicked.connect(self.open_fits_directory)
-        self.button_layout.addWidget(self.open_directory_button)
+    def create_image_tab(self):
+        """
+        Creates and configures the image tab.
+        """
+        self.image_tab = QWidget()
+        self.image_tab_layout = QVBoxLayout()
+        self.image_tab.setLayout(self.image_tab_layout)
 
-        # Create the Match Mode button
-        self.match_mode_button = QPushButton("Match Mode")
-        self.match_mode_button.setCheckable(True)  # Allow the button to be toggled
-        self.match_mode_button.toggled.connect(self.toggle_match_mode)
-        self.button_layout.addWidget(self.match_mode_button)
+        self.create_image_splitter()
+        self.create_contrast_slider()
 
-        # Add the button layout to the main layout
-        self.main_layout.addLayout(self.button_layout)
+        self.tab_widget.addTab(self.image_tab, "Images")
 
-        # Create and add the image label and slider
+    def create_image_splitter(self):
+        """
+        Creates and configures the image splitter and its components.
+        """
+        self.splitter = QSplitter(Qt.Vertical)
+        self.splitter.setVisible(True)
+        self.image_tab_layout.addWidget(self.splitter)
+
+        self.create_image_layout()
+        self.create_result_image_widget()
+
+        self.image_splitter_widget = QWidget()
+        self.image_splitter_layout = QVBoxLayout()
+        self.image_splitter_widget.setLayout(self.image_splitter_layout)
+        self.image_splitter_layout.addLayout(self.image_layout)
+
+        self.splitter.addWidget(self.image_splitter_widget)
+        self.splitter.addWidget(self.result_image_widget)
+
+    def create_image_layout(self):
+        """
+        Creates and configures the layout for images.
+        """
+        self.image_layout = QVBoxLayout()
+        self.image_labels_layout = QHBoxLayout()
         self.image_label1 = QLabel()
         self.image_label2 = QLabel()
+        self.image_labels_layout.addWidget(self.image_label1)
+        self.image_labels_layout.addWidget(self.image_label2)
 
-        #self.image_layout = QHBoxLayout()
-        #self.image_layout.addWidget(self.image_label1)
-        #self.image_layout.addWidget(self.image_label2)
-        #self.main_layout.addLayout(self.image_layout)
+        self.image_layout.addLayout(self.image_labels_layout)
 
-        # Create the splitter for match mode
-        self.splitter = QSplitter(Qt.Horizontal)
-        self.splitter.addWidget(self.image_label1)
-        self.splitter.addWidget(self.image_label2)
+    def create_result_image_widget(self):
+        """
+        Creates and configures the result image widget.
+        """
+        self.result_label = QLabel()
+        self.result_label.setFixedSize(1000, 500)
+        self.result_label.setAlignment(Qt.AlignCenter)
 
-        # Set initial sizes for both images
-        self.splitter.setSizes([self.width() // 2, self.width() // 2])
+        self.result_image_widget = QWidget()
+        self.result_image_widget.setStyleSheet("background-color: #E5E4E2;")
+        self.result_image_layout = QVBoxLayout()
+        self.result_image_layout.setAlignment(Qt.AlignCenter)
+        self.result_image_widget.setLayout(self.result_image_layout)
+        self.result_image_layout.addWidget(self.result_label)
 
-        # Add the splitter to the main layout
-        self.main_layout.addWidget(self.splitter)
-
+    def create_contrast_slider(self):
+        """
+        Creates and configures the contrast adjustment slider.
+        """
         self.slider = QSlider(Qt.Horizontal)
-        self.slider.setMinimum(0)
-        self.slider.setMaximum(100)
-        self.slider.setValue(50)  # Default value for contrast
-        self.slider.setTickInterval(10)
+        self.slider.setRange(0, 100)
+        self.slider.setValue(50)
         self.slider.setTickPosition(QSlider.TicksBelow)
+        self.slider.setTickInterval(10)
+        self.slider.setSingleStep(1)
+        self.slider.setToolTip("Adjust contrast")
+
+        self.image_tab_layout.addWidget(self.slider)
         self.slider.valueChanged.connect(self.adjust_contrast)
-        self.main_layout.addWidget(self.slider)
 
-        # Create and add the FITS header button and text area
-        self.header_button = QPushButton("View FITS Header")
-        self.header_button.clicked.connect(self.show_fits_header)
-        self.button_layout.addWidget(self.header_button)
+    def create_header_tab(self):
+        """
+        Creates and configures the header tab.
+        """
+        self.header_tab = QWidget()
+        self.header_layout = QVBoxLayout()
+        self.header_tab.setLayout(self.header_layout)
 
-        # Create and add the FITS header text areas
+        self.create_header_text_areas()
+
+        self.tab_widget.addTab(self.header_tab, "Header")
+
+    def create_header_text_areas(self):
+        """
+        Creates and configures the FITS header text areas.
+        """
         self.header_label1 = QLabel("Header 1:")
         self.header_text_area1 = QTextEdit()
         self.header_text_area1.setReadOnly(True)
@@ -90,94 +142,86 @@ class FITSViewerUI(QMainWindow):
         self.header_label2 = QLabel("Header 2:")
         self.header_text_area2 = QTextEdit()
         self.header_text_area2.setReadOnly(True)
-        self.header_label2.setVisible(False)
-        self.header_text_area2.setVisible(False)
 
-        self.header_layout = QVBoxLayout()
         self.header_layout.addWidget(self.header_label1)
         self.header_layout.addWidget(self.header_text_area1)
         self.header_layout.addWidget(self.header_label2)
         self.header_layout.addWidget(self.header_text_area2)
 
-        self.main_layout.addLayout(self.header_layout)
-
-        # Initialize both image labels to be empty
-        self.image_label1.setPixmap(QPixmap())
-        self.image_label2.setPixmap(QPixmap())
-
-    def toggle_match_mode(self, checked):
+    def create_menu_bar(self):
         """
-        Toggles the match mode on and off.
+        Creates and configures the menu bar.
         """
-        self.match_mode = checked
-        if self.match_mode:
-            self.match_mode_button.setStyleSheet("background-color: lightgreen;")  # Change button color when match mode is enabled
-            self.splitter.setVisible(True)  # Show the splitter in match mode
-            self.header_label2.setVisible(True)
-            self.header_text_area2.setVisible(True)
+        self.menu_bar = self.menuBar()
+        self.create_file_menu()
+        self.create_tools_menu()
+
+    def create_file_menu(self):
+        """
+        Creates and configures the File menu.
+        """
+        self.file_menu = self.menu_bar.addMenu("File")
+
+        self.open_file_action = QAction("Open FITS Image", self)
+        self.open_file_action.triggered.connect(self.open_fits_image)
+        self.file_menu.addAction(self.open_file_action)
+
+        self.open_directory_action = QAction("Open Directory of FITS Images", self)
+        self.open_directory_action.triggered.connect(self.open_fits_directory)
+        self.file_menu.addAction(self.open_directory_action)
+
+    def create_tools_menu(self):
+        """
+        Creates and configures the Tools menu.
+        """
+        self.tools_menu = self.menu_bar.addMenu("Tools")
+
+        self.match_mode_action = QAction("Match Mode", self)
+        self.match_mode_action.setCheckable(True)
+        self.match_mode_action.toggled.connect(self.toggle_match_mode)
+        self.tools_menu.addAction(self.match_mode_action)
+
+        self.subtract_signal_action = QAction("Subtract Signal", self)
+        self.subtract_signal_action.triggered.connect(self.subtract_from_images)
+        self.tools_menu.addAction(self.subtract_signal_action)
+
+        self.show_header_action = QAction("Show Header", self)
+        self.show_header_action.triggered.connect(self.show_header_tab)
+        self.tools_menu.addAction(self.show_header_action)
+
+    def toggle_header_visibility(self):
+        """
+        Toggles the visibility of the header section.
+        """
+        # Check if the header section is currently visible
+        is_visible = self.header_label1.isVisible()
+
+        # Set visibility based on the current state
+        new_visibility = not is_visible
+        self.header_label1.setVisible(new_visibility)
+        self.header_text_area1.setVisible(new_visibility)
+        self.header_label2.setVisible(new_visibility)
+        self.header_text_area2.setVisible(new_visibility)
+
+        # Update the button text to reflect the current state
+        if new_visibility:
+            self.show_header_button.setText("Hide Header")
         else:
-            self.match_mode_button.setStyleSheet("background-color: lightgray;")  # Change button color when match mode is disabled
-            self.splitter.setVisible(False)
-            self.header_label2.setVisible(False)
-            self.header_text_area2.setVisible(False)
+            self.show_header_button.setText("Show Header")
 
-    def adjust_layout_for_match_mode(self):
+    def show_header_tab(self):
         """
-        Adjusts the layout to display images side by side in match mode.
+        Switches to the Header tab in the tab widget.
         """
-        if self.match_mode:
-          self.image_label1.setVisible(True)
-          self.image_label2.setVisible(True)
-          if self.cached_images[0] is not None and self.cached_images[1] is not None:
-              # Calculate the combined size based on the larger dimension
-              img1_size = self.cached_images[0].size()
-              img2_size = self.cached_images[1].size()
-              combined_width = img1_size.width() + img2_size.width()
-              combined_height = max(img1_size.height(), img2_size.height())
-              self.resize(combined_width, combined_height)  # Resize the window to fit both images
-          else:
-              self.resize(1000, 1000)  # Default size or handle as needed
-        else:
-          # Single image mode
-          self.image_label1.setVisible(True)
-          self.image_label2.setVisible(False)  # Hide the second label
-          self.resize(self.image_label1.pixmap().size())  # Resize to fit the single image
+        self.tab_widget.setCurrentWidget(self.header_tab)
 
-    def scale_pixmap(self, pixmap, max_width, max_height):
-        """
-        Scales the QPixmap to fit within the specified maximum width and height while preserving the aspect ratio.
-        """
-        # Get the original size of the pixmap
-        original_size = pixmap.size()
+    def open_fits_image(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open FITS File", "", "FITS Files (*.fits)")
+        if file_name:
+            self.open_fits_image(file_name)
 
-        # Calculate the scaling factors
-        scale_x = max_width / original_size.width()
-        scale_y = max_height / original_size.height()
-
-        # Choose the smaller scaling factor to ensure the pixmap fits within the max dimensions
-        scale = min(scale_x, scale_y)
-
-        # Scale the pixmap
-        scaled_pixmap = pixmap.scaled(original_size * scale, Qt.KeepAspectRatio, Qt.SmoothTransformation)
-
-        return scaled_pixmap
-
-    def adjust_contrast(self):
-        """
-        Adjusts the contrast of the displayed image based on the slider value.
-        """
-        if self.original_image is not None:
-          # Get the contrast value from the slider
-          contrast = self.slider.value() / 50.0  # Scale factor, default is 1.0
-
-          # Adjust the contrast
-          min_val = np.min(self.original_image)
-          adjusted_image = np.clip(min_val + (self.original_image - min_val) * contrast, 0, 255).astype(np.uint8)
-
-          # Convert the adjusted image to QImage and then to QPixmap
-          q_image = self.convert_to_qimage(adjusted_image)
-          pixmap = QPixmap.fromImage(q_image)
-
-          # Display the adjusted image
-          self.display_image(pixmap)
+    def open_fits_directory(self):
+        directory = QFileDialog.getExistingDirectory(self, "Open Directory")
+        if directory:
+            self.open_fits_directory(directory)
 
